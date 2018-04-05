a7e0ec6004d8f9129caa62e4dc5fce453214d6f1
kha vo <daddylonglegs2014@gmail.com>
2018-04-05 23:17:52 +0700

    added new feature: auto resize for mobile screen

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem "jekyll-theme-midnight"
 group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.6"
   gem "jekyll-sitemap"
+  gem "uglifier"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ _includes/main.js
@@ -0,0 +1,11 @@
+<script>
+// alert("Hello you");
+var w = window,
+d = document,
+e = d.documentElement,
+g = d.getElementsByTagName('body')[0],
+x = w.innerWidth || e.clientWidth || g.clientWidth,
+y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+//alert(x + ' × ' + y);
+
+</script>
